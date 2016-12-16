c993496b5879b500b9169d636b7cbee579af71e0
Can Özsayar <c.ozsayar@googlemail.com>
2016-12-16 10:51:02 +0100

    .

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+  var sidemenu_isAnimating = false;
   // ##############################
   // PAGE NAVIGATION ELEMENTS
   // ##############################
@@ -24,5 +24,23 @@ $(document).ready(function() {
    }, 700, 'easeInOutSine');
    return false;
  });
+ window.addEventListener('scroll', function() {
+
+   //$("#sm_btn_contact").stop().animate({"top": (Math.floor((($(".panel").eq(2).offset().top  - $('body').scrollTop()) / $(document).height() * 100) + 50) + "%")});
+     if ($('body').scrollTop() > $(".panel").eq(1).offset().top - 500) {
+       $("#sidemenu").stop().animate({"left" : "0"}, 200);
+     }
+
+     else
+     {
+       $("#sidemenu").stop().animate({"left" : -$("#sidemenu").width()}, 200);
+     }
+ });
+
+
+
+
+
+
 
 });
